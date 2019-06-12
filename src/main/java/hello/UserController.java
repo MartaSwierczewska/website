8b2d28412d7c383568cf9bbcaae68a2622417e80
Marta <martaswierczewska@wp.pl>
2019-06-12 15:32:45 +0200

    main page finished

--- src/main/java/hello/UserController.java
+++ src/main/java/hello/UserController.java
@@ -3,7 +3,6 @@ package hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,8 +27,8 @@ public class UserController {
     }
 
     @GetMapping("/editUser/{id}")
-    public String editUser(@PathVariable("id") Long id, Model model){
-        User user=userService.getUserByID(id);
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        User user = userService.getUserByID(id);
         model.addAttribute("user", user);
         return "editUser";
     }
